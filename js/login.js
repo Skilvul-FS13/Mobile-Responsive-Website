830c2545262966a7b5bfea60181359560d27819a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-10 18:32:00 +0700

    feat: simple login authentication

---
+++ js/login.js
@@ -0,0 +1,45 @@
+// login page
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const error = document.getElementById('error');
+
+const auth = localStorage.getItem('isLoggedIn');
+
+if (auth) {
+  window.location.replace('/');
+  document.body.innerHTML = 'loading';
+}
+
+window.addEventListener('load', () => {
+  function getAccount() {
+    fetch('https://651d09d644e393af2d590b6d.mockapi.io/api/v1/account')
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        findUserAccount(users);
+      });
+  }
+
+  function findUserAccount(users) {
+    form.addEventListener('submit', (e) => {
+      console.log('🚀 ~ file: login.js:14 ~ .then ~ users:', users);
+      e.preventDefault();
+      const usernameInput = username.value;
+      const passwordInput = password.value;
+
+      const user = users.find((user) => user.username === usernameInput && user.password === passwordInput);
+
+      if (user) {
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'index.html';
+      } else {
+        error.innerHTML = `<div>invalid username or password</div>`;
+      }
+    });
+  }
+
+  getAccount();
+});
