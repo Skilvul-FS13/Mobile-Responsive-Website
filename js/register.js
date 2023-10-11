35e924fcc519dd9efd22575253c99077b4b1ee24
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-11 07:49:21 +0700

    feat: add POST method in fetch data

--- js/register.js
+++ js/register.js
@@ -13,13 +13,13 @@ if (auth) {
 }
 
 window.addEventListener('load', () => {
-  function createNewAccount(data) {
-    const accountKey = 'ACCOUNT_KEY';
-    let account = JSON.parse(localStorage.getItem(accountKey)) || [];
+  // function createNewAccount(data) {
+  //   const accountKey = 'ACCOUNT_KEY';
+  //   let account = JSON.parse(localStorage.getItem(accountKey)) || [];
 
-    account.unshift(data);
-    localStorage.setItem(accountKey, JSON.stringify(account));
-  }
+  //   account.unshift(data);
+  //   localStorage.setItem(accountKey, JSON.stringify(account));
+  // }
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -30,7 +30,18 @@ window.addEventListener('load', () => {
       password: passwordInput,
     };
 
+    fetch('https://651d09d644e393af2d590b6d.mockapi.io/api/v1/account', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newAccount),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log('data has been created', data))
+      .catch((error) => console.log('data failed', error));
+
     createNewAccount(newAccount);
-    window.location.replace('/');
+    // window.location.replace('/');
   });
 });
