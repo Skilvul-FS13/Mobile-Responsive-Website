8e9340a92b7e21b274d0132667960763a7ce1e71
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-12 14:36:00 +0700

    script refactor

--- script.js
+++ script.js
@@ -1,152 +1,68 @@
-// sponsored carousel
-const arrowButton = document.querySelectorAll('.carousel #left,#right');
-const sponsored = document.querySelector('.sponsor-list');
-const sponsorList = sponsored.querySelectorAll('.sponsor');
-const sponsorListWidth = sponsored.querySelectorAll('.sponsor')[0].offsetWidth;
+// navbar shadow
+const navbar = document.querySelector('.navbar-container');
 
-let currentIndex = 0;
-
-arrowButton.forEach((button) => {
-  button.addEventListener('click', () => {
-    currentIndex += button.id === 'left' ? -1 : 1;
-    scrollToCurrentSponsorIndex();
-  });
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar-shadow');
+  } else {
+    navbar.classList.remove('navbar-shadow');
+  }
 });
 
-function scrollToCurrentSponsorIndex() {
-  const maxIndex = sponsorList.length + 2;
+// show dropdown
+const navInformation = document.querySelector('#information');
+const dropdown = document.querySelector('.dropdown');
 
-  if (currentIndex <= 0) {
-    currentIndex = maxIndex;
-  } else if (currentIndex >= maxIndex) {
-    currentIndex = 0;
-  }
-
-  const scrollAmount = currentIndex * sponsorListWidth;
-  sponsored.scrollTo({
-    left: scrollAmount,
-    behavior: 'smooth',
-  });
-}
-
-setInterval(() => {
-  currentIndex++;
-  scrollToCurrentSponsorIndex();
-}, 3000);
-
-// news card slider
-const newsList = document.querySelector('.news-list');
-const newsListWidth = newsList.querySelector('.news-card').offsetWidth;
-
-arrowButton.forEach((button) => {
-  button.addEventListener('click', () => {
-    newsList.scrollLeft += button.id === 'left' ? -newsListWidth : newsListWidth;
-  });
+navInformation.addEventListener('click', () => {
+  navInformation.classList.toggle('active');
+  dropdown.classList.toggle('show-dropdown');
 });
 
-const berita = [
-  {
-    img: 'news1.jpeg',
-    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-  },
-  {
-    img: 'news1.jpeg',
-    title: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi doloremque deleniti nequ ',
-  },
-  {
-    img: 'news1.jpeg',
-    title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi doloremque deleniti neque Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi.',
-  },
-  {
-    img: 'news1.jpeg',
-    title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi doloremque deleniti neque Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi.',
-  },
-  {
-    img: 'news1.jpeg',
-    title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi doloremque deleniti neque Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi.',
-  },
-  {
-    img: 'news1.jpeg',
-    title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi doloremque deleniti neque Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi.',
-  },
-  {
-    img: 'news1.jpeg',
-    title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi doloremque deleniti neque Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia sint maxime natus a esse vitae accusantium, consequatur rerum quia laboriosam saepe tempore quasi sed ullam. Nesciunt modi.',
-  },
-];
+// sidebar mobile
+const sidebar = document.querySelector('.sidebar');
+const burgerButton = document.querySelector('.nav-burger');
+const overlay = document.querySelector('.overlay');
+const dropdownSidebar = document.querySelector('.dropdown-sidebar #information');
+const dropdownSidebarList = document.querySelector('.dropdown-sidebar-list');
 
-const listBerita = berita.map((item) => showNews(item)).join('');
-newsList.innerHTML = listBerita;
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('active');
+  sidebar.classList.toggle('active');
+  overlay.classList.toggle('active');
+});
 
-function showNews(item) {
-  return `
-  <div class="news-card">
-    <div class="news-image">
-      <img src="./img/${item.img}" alt="" loading="lazy" />
-    </div>
-    <div class="news-card-content">
-      <p>Senin, 28 Agu 2023 18:30 WIB</p>
-      <h4>${item.title}</h4>
-      <a href="/">Baca Selengkapnya</a>
-    </div>
-  </div>`;
+overlay.addEventListener('click', () => {
+  burgerButton.classList.toggle('active');
+  sidebar.classList.toggle('active');
+  overlay.classList.toggle('active');
+});
+
+dropdownSidebar.addEventListener('click', () => {
+  dropdownSidebarList.classList.toggle('active');
+});
+
+// navbar dynamic
+const auth = localStorage.getItem('isLoggedIn', 'USER_LOGGED_IN');
+const navbarButtonLogin = document.querySelector('.nav-button-login');
+
+const sidebarButtonLogin = document.querySelector('.login-sidebar');
+
+if (auth) {
+  navbarButtonLogin.innerHTML = buttonLogOut();
+  sidebarButtonLogin.innerHTML = buttonLogOut();
 }
 
-const btnSubmit = document.getElementById('submit-form');
-const showResult = document.getElementById('result');
-const form = document.getElementById('form');
-
-function handleGetFormData() {
-  const name = document.getElementById('name').value;
-  const city = document.getElementById('city').value;
-  const email = document.getElementById('email').value;
-  const zipCode = document.getElementById('zip-code').value;
-  const status = document.getElementById('status').checked;
-
-  const formData = {
-    name,
-    city,
-    email,
-    zipCode,
-    status,
-  };
-
-  return formData;
+function buttonLogOut() {
+  return `<a class="btn-primary" id="logout">Log Out</a>`;
 }
 
-function isNumber(string) {
-  return !isNaN(string);
-}
+const setLogOut = document.querySelectorAll('#logout');
 
-function checkboxIsChecked() {
-  const status = document.getElementById('status').checked;
-  return status ? true : false;
-}
-
-function validateFormData(formData) {
-  if (formData.name !== '' && formData.city !== '' && formData.email !== '' && isNumber(formData.zipCode) && checkboxIsChecked()) {
-    console.log('form terisi');
-    return true;
-  } else {
-    console.log('tidak terisi');
-    return false;
-  }
-}
-
-form.addEventListener('submit', submit);
-
-function submit(e) {
-  e.preventDefault();
-  const data = handleGetFormData();
-  const warning = document.getElementById('warning');
-  if (validateFormData(data) === false) {
-    warning.textContent = 'Periksa form anda sekali lagi';
-  } else {
-    warning.textContent = '';
-  }
+if (setLogOut) {
+  setLogOut.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      localStorage.removeItem('isLoggedIn');
+      window.location.replace('/');
+    });
+  });
 }
