d05acca588ef35a940f526241cda936fc635af24
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-14 10:41:03 +0700

    added context.js

---
+++ js/utils/context.js
@@ -0,0 +1,22 @@
+// * API KEY from MOCKAPI
+// * Userpost Response
+const USER_POST_API = 'https://651d09d644e393af2d590b6d.mockapi.io/api/v1/userPost';
+// * User Accout Response
+const USER_API = 'https://651d09d644e393af2d590b6d.mockapi.io/api/v1/account';
+
+// format Date
+const formatDate = (date) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+    timeZone: 'Asia/Jakarta',
+  };
+
+  return new Date(date).toLocaleDateString('id-ID', options);
+};
+
+export { formatDate, USER_POST_API, USER_API };
