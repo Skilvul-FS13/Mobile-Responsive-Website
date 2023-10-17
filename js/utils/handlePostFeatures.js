ee092fcbfa4a0de54f1daaa61a1c29edf0fd5d9a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-17 23:41:48 +0700

    like button feature

--- js/utils/handlePostFeatures.js
+++ js/utils/handlePostFeatures.js
@@ -1,31 +1,98 @@
 export function handlePostFeatures(postById) {
-  console.log('🚀 ~ file: handlePostFeatures.js:2 ~ handlePostFeatures ~ postById:', postById);
-  // get all like buttons
+  // // console.log('🚀 ~ file: handlePostFeatures.js:2 ~ handlePostFeatures ~ postById:', postById);
+  // get all dom selectors
   const likeButton = document.querySelectorAll('#like-button');
-
-  // get all comment buttons
   const commentButton = document.querySelectorAll('#comment');
-
-  // get all share buttons
   const shareButton = document.querySelectorAll('#share-button');
-  likeButton.forEach((btn) => {
-    btn.addEventListener('click', (event) => handleUserIsLoggedIn());
+  const postElements = document.querySelectorAll('.post-box');
+  const allLikeCount = document.querySelectorAll('.like-count');
+
+  // get each likebutton by giving btn param, and index to get each elements
+  likeButton.forEach((btn, index) => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      const postElement = postElements[index];
+      const postId = postElement.getAttribute('data-post-id');
+      const likeCount = allLikeCount[index];
+      // // console.log('🚀 ~ file: handlePostFeatures.js:19 ~ btn.addEventListener ~ likeCount:', likeCount);
+
+      // get each button by giving btn.innerhtml.trim() so it can detect which button like is false or true
+      if (btn.innerHTML.trim() == '<i class="bi bi-tree-fill"></i>') {
+        let isLiked = false;
+        handleUserUnlikes(postId, likeCount, isLiked);
+        likeCount.innerHTML = parseInt(likeCount.textContent) - 1;
+        btn.innerHTML = '<i class="bi bi-tree"></i>';
+      } else {
+        let isLiked = true;
+        handleUserLikes(postId, likeCount, isLiked);
+        likeCount.innerHTML = parseInt(likeCount.textContent) + 1;
+        btn.innerHTML = '<i class="bi bi-tree-fill"></i>';
+      }
+    });
   });
 
+  // get all comment buittons
   commentButton.forEach((btn) => {
     btn.addEventListener('click', (event) => handleFeatureNotAvailable());
   });
+
+  // get all share buttons
   shareButton.forEach((btn) => {
     btn.addEventListener('click', (event) => handleFeatureNotAvailable());
   });
 
   // handle user must login before using like feature
-  function handleUserIsLoggedIn(event) {
+  function handleUserLikes(postId, likeCount, isLiked) {
+    // // console.log('🚀 ~ file: handlePostFeatures.js:39 ~ handleUserLikes ~ postId:', postId);
     const IS_LOGGED_IN_KEY = 'isLoggedIn';
     const isLoggedIn = localStorage.getItem(IS_LOGGED_IN_KEY);
 
     if (isLoggedIn) {
-      console.log('nice');
+      fetch(`https://651d09d644e393af2d590b6d.mockapi.io/api/v1/userPost/${postId}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          likes: parseInt(likeCount.innerHTML) + 1,
+          isLiked: isLiked,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        // get all data api and store it to localStorage
+        .then((data) => {
+          // // console.log('🚀 ~ file: handlePostFeatures.js:67 ~ handleUserLikes ~ data:', data);
+          return localStorage.setItem('DATA_KEY', JSON.stringify(data));
+        });
+    } else {
+      const toastContainer = document.querySelector('.toast-container');
+      toastContainer.innerHTML += getNotification();
+      hideNextIndex(0);
+    }
+  }
+
+  // handle user to unlike with condition if user is logged in
+  function handleUserUnlikes(postId, likeCount, isLiked) {
+    const IS_LOGGED_IN_KEY = 'isLoggedIn';
+    const isLoggedIn = localStorage.getItem(IS_LOGGED_IN_KEY);
+
+    if (isLoggedIn) {
+      fetch(`https://651d09d644e393af2d590b6d.mockapi.io/api/v1/userPost/${postId}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          likes: parseInt(likeCount.innerHTML) - 1,
+          isLiked: isLiked,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        // get all data api and store it to localStorage
+        .then((data) => {
+          // // console.log('🚀 ~ file: handlePostFeatures.js:96 ~ handleUserUnlikes ~ data:', data);
+          return localStorage.setItem('DATA_KEY', JSON.stringify(data));
+        });
     } else {
       const toastContainer = document.querySelector('.toast-container');
       toastContainer.innerHTML += getNotification();
@@ -35,18 +102,13 @@ export function handlePostFeatures(postById) {
 
   // handle user must login before using comment feature
   function handleFeatureNotAvailable(event) {
-    const IS_LOGGED_IN_KEY = 'isLoggedIn';
-    const isLoggedIn = localStorage.getItem(IS_LOGGED_IN_KEY);
+    const toastContainer = document.querySelector('.toast-container');
+    toastContainer.innerHTML += getNotificationFeatures();
 
-    if (isLoggedIn) {
-      console.log('nice');
-    } else {
-      const toastContainer = document.querySelector('.toast-container');
-      toastContainer.innerHTML += getNotificationFeatures();
-      hideNextIndex(0);
-    }
+    hideNextIndex(0);
   }
 
+  // handle hide next notification after 1 second
   const hideNextIndex = (index) => {
     const toasts = document.querySelectorAll('.toast');
     const currentNotif = toasts[index];
@@ -55,7 +117,7 @@ export function handlePostFeatures(postById) {
     btnClose.forEach((close) => {
       close.addEventListener('click', () => {
         for (const toast of toasts) {
-          toast.classList.add('d-none');
+          toast.remove();
         }
       });
     });
@@ -69,7 +131,7 @@ export function handlePostFeatures(postById) {
       return;
     }
     setTimeout(() => {
-      currentNotif.classList.add('d-none');
+      currentNotif.remove();
       hideNextIndex(index + 1);
     }, 1000);
   };
@@ -86,7 +148,7 @@ export function handlePostFeatures(postById) {
     </div>`;
   }
 
-  // Toast notification that you aren't login yet
+  // Toast notification that features arent available yet
   function getNotificationFeatures() {
     return `
     <div class="toast d-block" role="alert" aria-live="assertive" aria-atomic="true">
